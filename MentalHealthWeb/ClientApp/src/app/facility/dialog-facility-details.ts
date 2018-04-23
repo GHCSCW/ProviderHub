8b1db86ce804df23c9ede586b77739dfda802975
Alex Peasley <apeasley@ghcscw.com>
2018-04-23 15:55:21 -0500

    Found a better strategy for resetting dialog details. Added a close button to all the dialogs.

--- MentalHealthWeb/ClientApp/src/app/facility/dialog-facility-details.ts
+++ MentalHealthWeb/ClientApp/src/app/facility/dialog-facility-details.ts
@@ -14,7 +14,7 @@ import {MentalHealthService } from '../services/mental.health.service';
 export class DialogFacilityDetails {
 
   @Input() facility: any[];
-  @Input() originalFacility: any[];
+
   constructor(public dialog: MatDialog) { }
 
   openDialog(): void {
@@ -22,7 +22,7 @@ export class DialogFacilityDetails {
       width: '500px',
       height: '850px',
 
-      data: { facility: this.facility, originalFacility: this.originalFacility }
+      data: { facility: this.facility}
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -37,22 +37,27 @@ export class DialogFacilityDetails {
 })
 export class DialogFacilityDetailsDialog {
 
+  originalFacility: any = []; 
   constructor(private mentalHealthService: MentalHealthService,
     public dialogRef: MatDialogRef<DialogFacilityDetailsDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+
+    this.originalFacility = JSON.parse(JSON.stringify(data.facility));
+    dialogRef.disableClose = true;
+  }
 
   states = this.mentalHealthService.getStates();
 
   onNoClick(): void {
 
-    Object.assign(this.data.facility, this.data.originalFacility);
+    Object.assign(this.data.facility, this.originalFacility);
     this.dialogRef.close();
   }
 
   onSubmit() {
 
     this.mentalHealthService.updateFacility(this.data.facility).subscribe(updateFacility => {
-      Object.assign(this.data.originalFacility, updateFacility);
+      Object.assign(this.data.facility, updateFacility);
 
     });
     this.dialogRef.close();
