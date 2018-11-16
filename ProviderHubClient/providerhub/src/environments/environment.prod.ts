40cb7a98b91e1527ec45b433af1f9718fcbaf268
Pillai <spillai@ghcscw.com>
2018-11-16 11:14:13 -0600

    ng prod config change

--- ProviderHubClient/providerhub/src/environments/environment.prod.ts
+++ ProviderHubClient/providerhub/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiRoot: "http://webappd1:200/",
-  authUser: {result:"",username:"",isSuperUser:false,isEditor:false,isUser:false}
+  authUser: { result: "", username: "", isSuperUser: "False", isEditor: "False", isUser: "False"}
 };
